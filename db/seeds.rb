8f4d57ee435eb5328107edb7cb14c20ee453e7db
Kana28 <kaichum24@gmail.com>
2021-04-25 12:46:44 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Genre.create!(name: '相談')
+Genre.create!(name: 'おすすめ')
