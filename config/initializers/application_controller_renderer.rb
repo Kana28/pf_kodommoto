5ca1fd06ee071c7597422ea3bbf2cb0a4c5551c7
Kana28 <kaichum24@gmail.com>
2021-04-25 11:53:55 +0000

    key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = 'e614564c2891a18df150712a1d9d317eb7a901047bf100a79637c8ca55ae7acc604e3a2e3dab2de82aa43574472ed8c6b998c465b87c860e4b4e1ca39ed6c308'
